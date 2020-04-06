7be2e8093fc51905cc2fdf5dbf4e3fdee1b3a48b
Zanetta Norris <zanettanorris@gmail.com>
2020-04-06 13:05:09 -0400

    added to object oriented. 89  pass.

--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/PalindromeObject.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/PalindromeObject.java
@@ -1,21 +1,28 @@
 package rocks.zipcode.io.quiz4.objectorientation;
 
+import rocks.zipcode.io.quiz4.fundamentals.PalindromeEvaluator;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class PalindromeObject {
+    String string;
+
     public PalindromeObject(String input) {
+         string = input;
     }
 
     public String[] getAllPalindromes(){
-        return null;
+
+        return PalindromeEvaluator.getAllPalindromes(string);
     }
 
     public Boolean isPalindrome(){
-        return null;
+
+        return PalindromeEvaluator.isPalindrome(string);
     }
 
     public String reverseString(){
-        return null;
+        return PalindromeEvaluator.reverseString(string);
     }
 }
--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringEvaluatorObject.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringEvaluatorObject.java
@@ -1,14 +1,18 @@
 package rocks.zipcode.io.quiz4.objectorientation;
 
+import rocks.zipcode.io.quiz4.fundamentals.StringEvaluator;
+
 /**
  * @author leon on 19/12/2018.
  */
 public class StringEvaluatorObject {
+    String string;
     public StringEvaluatorObject(String str) {
+        string = str;
     }
 
     public String[] getAllSubstrings() {
-        return null;
+        return StringEvaluator.getAllSubstrings(string);
     }
 
     public String[] getCommonSubstrings(String secondInput) {
