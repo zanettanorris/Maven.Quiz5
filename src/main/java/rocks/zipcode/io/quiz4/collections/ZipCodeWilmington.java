64c4c9b4783587f326b1325ca3fe11cee099d5d4
Zanetta Norris <zanettanorris@gmail.com>
2020-04-06 12:55:34 -0400

    added code to Student and ZCW. 68 test pass.

--- src/main/java/rocks/zipcode/io/quiz4/collections/ZipCodeWilmington.java
+++ src/main/java/rocks/zipcode/io/quiz4/collections/ZipCodeWilmington.java
@@ -1,22 +1,31 @@
 package rocks.zipcode.io.quiz4.collections;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
  * @author leon on 11/12/2018.
  */
 public class ZipCodeWilmington {
+    Map <Student, Double> studyMap = new LinkedHashMap<>();
     public void enroll(Student student) {
-    }
+    studyMap.put(student, student.getTotalStudyTime()); }
 
     public Boolean isEnrolled(Student student) {
-        return null;
+        return studyMap.containsKey(student);
     }
 
     public void lecture(double numberOfHours) {
+        for (Student student: studyMap.keySet()){
+            student.learn(numberOfHours);
+        }
     }
 
     public Map<Student, Double> getStudyMap() {
-        return null;
+        for (Student student: studyMap.keySet()){
+            studyMap.put(student, student.getTotalStudyTime());
+        }
+        return studyMap;
     }
 }
